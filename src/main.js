ae189357e4107b2cb9626cf42ba77f76509f3fe6
Kateryna Kleshch <ketrins1993@gmail.com>
2025-07-09 20:12:28 +0300

    доробили

--- src/main.js
+++ src/main.js
@@ -1,3 +1,48 @@
 import {galleryItems} from "./js/galleryItems";
+import Handlebars from 'handlebars';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+
+// знаходимо посилання на список
+const galleryListEl = document.querySelector('.js-gallery');
+
+
+// Варіанти використання шаблонів
+// 1 варіант - додаємо теплейт в розмітку
+// const galleryTempl = document.querySelector('.gallery-teml');
+
+// const template = Handlebars.compile(galleryTempl.innerHTML);
+
+// const createGalleryItems = (arr) => {
+//     return arr.map(item => {
+//         return template(item);
+//     }).join(" ")
+// }
+
+
+
+// 2 variant - шаблон окремо в файлі імпортується
+import galleryTempl from './js/galleryTeml.hbs?raw';
+
+const template = Handlebars.compile(galleryTempl);
+
+const createGalleryItems = arr => {
+  return template(arr);
+};
+
+
+
+// створюємо список та додаємо в розмітку
+const markup = createGalleryItems(galleryItems);
+
+galleryListEl.innerHTML = markup;
+
+
+
+
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
